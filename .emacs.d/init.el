fab12713f8e950564c8a265dfec597277fb1be8b
Donald Ball <donald.ball@livingsocial.com>
2014-06-16 09:59:41 -0400

    Switch to solarized-theme

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -12,7 +12,7 @@
 		      starter-kit-eshell
 		      clojure-mode
 		      clojure-test-mode
-                      color-theme-solarized
+                      solarized-theme
                       cider))
 
 (dolist (p my-packages)
@@ -33,17 +33,17 @@
 (setq cider-popup-stacktraces nil)
 (add-hook 'cider-repl-mode-hook 'paredit-mode)
 
-(setq solarized-termcolors 16)
-(load-theme 'solarized-light t)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" default))))
+ '(custom-safe-themes (quote ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(load-theme 'solarized-light)
