9bc27486b0624f6ff79f8a6476c2aed94b0d59fb
Donald Ball <donald.ball@livingsocial.com>
2014-06-17 10:23:09 -0400

    Add clj-refactor to emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -48,3 +48,8 @@
  )
 
 (load-theme 'solarized-light)
+
+(require 'clj-refactor)
+(add-hook 'clojure-mode-hook (lambda ()
+                               (clj-refactor-mode 1)
+                               (cljr-add-keybindings-with-prefix "C-c C-m")))
