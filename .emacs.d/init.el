e19ea5664434b33bca2e29cc71b08037046a2f35
Donald Ball <donald.ball@livingsocial.com>
2014-06-18 15:34:58 -0400

    Broke all the emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -12,15 +12,12 @@
 		      starter-kit-eshell
                       clj-refactor
 		      clojure-mode
-		      clojure-test-mode
                       solarized-theme
                       cider))
 
 (dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p)))
-
-(add-hook 'clojure-mode-hook 'whitespace-mode)
 
 (setq backup-directory-alist
       `((".*" . ,temporary-file-directory)))
@@ -29,17 +26,23 @@
 
 (add-to-list 'auto-mode-alist '("\.cljs$" . clojure-mode))
 
-(setq nrepl-hide-special-buffers t)
-(setq cider-repl-pop-to-buffer-on-connect nil)
-(setq cider-popup-stacktraces nil)
-(add-hook 'cider-repl-mode-hook 'paredit-mode)
+(add-hook 'clojure-mode-hook 'whitespace-mode)
 
+(add-hook 'cider-repl-mode-hook 'paredit-mode)
+(setq nrepl-hide-special-buffers t)
+(setq cider-show-error-buffer 'except-in-repl)
+(setq cider-repl-print-length 100)
+
+(require 'clj-refactor)
+(add-hook 'clojure-mode-hook (lambda ()
+                               (clj-refactor-mode 1)
+                               (cljr-add-keybindings-with-prefix "C-c C-m")))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" default))))
+ '(custom-safe-themes (quote ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -48,8 +51,3 @@
  )
 
 (load-theme 'solarized-light)
-
-(require 'clj-refactor)
-(add-hook 'clojure-mode-hook (lambda ()
-                               (clj-refactor-mode 1)
-                               (cljr-add-keybindings-with-prefix "C-c C-m")))
